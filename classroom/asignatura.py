77b5fec2597f9bfdc2d2f4a009f82d07a614e7ca
Estebanmartinac1190 <yfpedraz@unal.edu.co>
2022-04-05 22:30:39 -0500

    :+1:

--- classroom/asignatura.py
+++
@@ -1,9 +0,0 @@
-class Asignatura:
-
-    def __init__(self, nombre, salon="remoto"):
-        self._nombre = nombre
-        self._salon = salon
-
-    def __str__(self):
-        cadena = self._nombre + ' ' + self._salon
-        return cadena
